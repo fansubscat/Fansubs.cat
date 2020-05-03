a90441a5782ec5ba6db995d43ab06f7f9039f130
Fansubs.cat <info@fansubs.cat>
2020-05-03 17:07:28 +0200

    Add custom Piwigo themes for manga site (with site chooser)

---
+++ manga_themes/fansubscat_computer/themeconf.inc.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Theme Name: fansubscat_computer
+Version: 1.0.0
+Description: Elegant theme adapted for Fansubs.cat.
+Theme URI: https://manga.fansubs.cat/
+Author: Fansubs.cat
+Author URI: https://manga.fansubs.cat/
+*/
+$themeconf = array(
+  'name'  => 'fansubscat_computer',
+  'parent' => 'elegant'
+);
+?>
