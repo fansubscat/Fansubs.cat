b7cdc1d7e6532920e641687555ff01c97b368b2a
Fansubs.cat <info@fansubs.cat>
2021-11-21 11:33:04 +0100

    Fix typo in recommendation query

--- services/generate_recommendations_cron.php
+++ services/generate_recommendations_cron.php
@@ -28,7 +28,7 @@ query("DELETE FROM manga_recommendation");
 //-Recommendations from the previous set
 //-Series with a score below 6.0
 query("INSERT INTO recommendation SELECT vr.id FROM version vr LEFT JOIN series sr ON vr.series_id=sr.id WHERE vr.hidden=0 AND (sr.type<>'movie' OR (sr.type='movie' AND sr.episodes=1) OR (sr.type='movie' AND (SELECT COUNT(vr2.id) FROM version vr2 WHERE vr2.series_id=vr.series_id)<=1)) AND (sr.rating<>'XXX' OR sr.rating IS NULL) AND (sr.score>=6 OR sr.score IS NULL OR vr.is_always_featured=1) AND vr.is_featurable=1 AND ((vr.status IN (1,3) AND vr.id NOT IN (".implode(',', $previous_ids_anime).")) OR vr.is_always_featured=1) AND vr.episodes_missing=0 ORDER BY vr.is_always_featured DESC, RAND() LIMIT 10");
-query("INSERT INTO manga_recommendation SELECT vr.id FROM manga_version vr LEFT JOIN manga mr ON vr.manga_id=mr.id WHERE vr.hidden=0 AND (mr.rating<>'XXX' OR mr.rating IS NULL) AND (mr.score>=6 PR mr.score IS NULL OR vr.is_always_featured=1) AND vr.is_featurable=1 AND ((vr.status IN (1,3) AND vr.id NOT IN (".implode(',', $previous_ids_manga).")) OR vr.is_always_featured=1) AND vr.chapters_missing=0 ORDER BY vr.is_always_featured DESC, RAND() LIMIT 10");
+query("INSERT INTO manga_recommendation SELECT vr.id FROM manga_version vr LEFT JOIN manga mr ON vr.manga_id=mr.id WHERE vr.hidden=0 AND (mr.rating<>'XXX' OR mr.rating IS NULL) AND (mr.score>=6 OR mr.score IS NULL OR vr.is_always_featured=1) AND vr.is_featurable=1 AND ((vr.status IN (1,3) AND vr.id NOT IN (".implode(',', $previous_ids_manga).")) OR vr.is_always_featured=1) AND vr.chapters_missing=0 ORDER BY vr.is_always_featured DESC, RAND() LIMIT 10");
 
 log_action('cron-recommendations-finished', "S'ha completat l'actualització periòdica de recomanacions");
 
