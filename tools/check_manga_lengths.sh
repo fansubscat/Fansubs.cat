68e4def7ccdbed7449e87cf67c05e0520efead0a
Fansubs.cat <info@fansubs.cat>
2022-12-28 00:14:34 +0100

    Fix manga length check script

--- tools/check_manga_lengths.sh
+++ tools/check_manga_lengths.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-storage_dir="/srv/websites/static.fansubs.cat/storage/"
+storage_dir="/YOUR/MANGA/STORAGE/DIR"
 token="YOUR_TOKEN"
 
 json=`curl https://api.fansubs.cat/internal/get_manga_files/?token=$token 2> /dev/null`
@@ -14,7 +14,7 @@ then
 		series=`echo $element | jq -r '.series'`
 		original_filename=`echo $element | jq -r '.original_filename'`
 		expected_length=`echo $element | jq -r '.length'`
-		real_length=`ls -1 $storage_dir/$file_id | wc -l`
+		real_length=`ls -1 "$storage_dir/$file_id" | wc -l`
 
 		if [ ! "$expected_length" == "$real_length" ]
 		then
