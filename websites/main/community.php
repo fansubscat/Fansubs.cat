7b66d7b2e509df396286f4498f052456caf95fcf
Fansubs.cat <info@fansubs.cat>
2023-10-22 20:17:49 +0200

    Community categories

--- websites/main/community.php
+++ websites/main/community.php
@@ -8,25 +8,30 @@ require_once("queries.inc.php");
 ?>
 					<div class="text-page">
 						<h2 class="section-title"><i class="fa fa-fw fa-people-group"></i> Comunitat</h2>
-						<div class="section-content">A banda dels diferents fansubs en català, la comunitat otaku en la nostra llengua és molt extensa. Hem cregut convenient fer un recull dels principals mitjans i creadors que fan contingut en català. Us convidem a seguir-los i donar-los suport!</div>
-						<h2 class="section-title"><i class="fa fa-fw fa-star"></i> Destacats</h2>
-						<div class="section-content community-container community-featured">
+						<div class="section-content">A banda dels diferents fansubs en català, la comunitat otaku en la nostra llengua és molt extensa. Hem cregut convenient fer un recull dels principals mitjans i creadors que fan contingut en català. Et convidem a seguir-los i donar-los suport!</div>
 <?php
-	$result = query_communities_by_category('featured');
-	while ($row = mysqli_fetch_assoc($result)){
-		print_community($row);
-	}
+	$categories = array(
+		'featured' => '<i class="fa fa-fw fa-star"></i> Destacats',
+		'creators' => '<i class="fa fa-fw fa-user"></i> Creadors',
+		'music' => '<i class="fa fa-fw fa-music"></i> Música',
+		'podcasts' => '<i class="fa fa-fw fa-radio"></i> Pòdcasts',
+	);
+	foreach ($categories as $id => $title) {
+		$result = query_communities_by_category($id);
+		if (mysqli_num_rows($result)>0) {
+?>
+						<h2 class="section-title"><?php echo $title; ?></h2>
+						<div class="section-content community-container<?php echo $id=='featured' ? ' community-featured' : ''; ?>">
+<?php
+			while ($row = mysqli_fetch_assoc($result)){
+				print_community($row);
+			}
 ?>
 						</div>
-						<h2 class="section-title"><i class="fa fa-fw fa-user"></i> Creadors</h2>
-						<div class="section-content community-container">
 <?php
-	$result = query_communities_by_category('creators');
-	while ($row = mysqli_fetch_assoc($result)){
-		print_community($row);
+		}
 	}
 ?>
-						</div>
 					</div>
 <?php
 require_once("../common.fansubs.cat/footer.inc.php");
