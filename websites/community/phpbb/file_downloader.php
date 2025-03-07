fcb9af2cb2c54763fea4d593dc8d99902ad2f0e6
Fansubs.cat <info@fansubs.cat>
2025-03-07 17:35:32 +0100

    Add phpBB 3.3.14

---
+++ websites/community/phpbb/file_downloader.php
@@ -0,0 +1,147 @@
+<?php
+/**
+*
+* This file is part of the phpBB Forum Software package.
+*
+* @copyright (c) phpBB Limited <https://www.phpbb.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+* For full copyright and license information, please see
+* the docs/CREDITS.txt file.
+*
+*/
+
+namespace phpbb;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use phpbb\exception\runtime_exception;
+
+class file_downloader
+{
+	const OK = 200;
+	const NOT_FOUND = 404;
+	const REQUEST_TIMEOUT = 408;
+
+	/** @var string Error string */
+	protected $error_string = '';
+
+	/** @var int Error number */
+	protected $error_number = 0;
+
+	/**
+	 * Create new guzzle client
+	 *
+	 * @param string $host
+	 * @param int $port
+	 * @param int $timeout
+	 *
+	 * @return Client
+	 */
+	protected function create_client(string $host, int $port = 443, int $timeout = 6): Client
+	{
+		// Only set URL scheme if not specified in URL
+		$url_parts = parse_url($host);
+		if (!isset($url_parts['scheme']))
+		{
+			$host = (($port === 443) ? 'https://' : 'http://') . $host;
+		}
+
+		// Initialize Guzzle client
+		return new Client([
+			'base_uri' => $host,
+			'timeout'  => $timeout,
+		]);
+	}
+
+	/**
+	 * Retrieve contents from remotely stored file
+	 *
+	 * @param string    $host            File host
+	 * @param string    $directory       Directory file is in
+	 * @param string    $filename        Filename of file to retrieve
+	 * @param int       $port            Port to connect to; default: 443
+	 * @param int       $timeout         Connection timeout in seconds; default: 6
+	 *
+	 * @return false|string File data as string if file can be read and there is no
+	 *         timeout, false if there were errors or the connection timed out
+	 *
+	 * @throws runtime_exception If data can't be retrieved and no error
+	 *         message is returned
+	 */
+	public function get(string $host, string $directory, string $filename, int $port = 443, int $timeout = 6)
+	{
+		// Initialize Guzzle client
+		$client = $this->create_client($host, $port, $timeout);
+
+		// Set default values for error variables
+		$this->error_number = 0;
+		$this->error_string = '';
+
+		try
+		{
+			$response = $client->request('GET', "$directory/$filename");
+
+			// Check if the response status code is 200 (OK)
+			if ($response->getStatusCode() == self::OK)
+			{
+				return $response->getBody()->getContents();
+			}
+			else
+			{
+				$this->error_number = $response->getStatusCode();
+				throw new runtime_exception('FILE_NOT_FOUND', [$filename]);
+			}
+		}
+		catch (RequestException $exception)
+		{
+			if ($exception->hasResponse())
+			{
+				$this->error_number = $exception->getResponse()->getStatusCode();
+
+				if ($this->error_number == self::NOT_FOUND)
+				{
+					throw new runtime_exception('FILE_NOT_FOUND', [$filename]);
+				}
+			}
+			else
+			{
+				$this->error_number = self::REQUEST_TIMEOUT;
+				throw new runtime_exception('FSOCK_TIMEOUT');
+			}
+
+			$this->error_string = utf8_convert_message($exception->getMessage());
+			return false;
+		}
+		catch (runtime_exception $exception)
+		{
+			// Rethrow runtime_exceptions, only handle unknown cases below
+			throw $exception;
+		}
+		catch (\Throwable $exception)
+		{
+			$this->error_string = utf8_convert_message($exception->getMessage());
+			throw new runtime_exception('FSOCK_DISABLED');
+		}
+	}
+
+	/**
+	 * Get error string
+	 *
+	 * @return string Error string
+	 */
+	public function get_error_string(): string
+	{
+		return $this->error_string;
+	}
+
+	/**
+	 * Get error number
+	 *
+	 * @return int Error number
+	 */
+	public function get_error_number(): int
+	{
+		return $this->error_number;
+	}
+}
---
+++ websites/community/vendor/twig/twig/src/Extension/SandboxExtension.php
@@ -0,0 +1,137 @@
+<?php
+
+/*
+ * This file is part of Twig.
+ *
+ * (c) Fabien Potencier
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Twig\Extension;
+
+use Twig\NodeVisitor\SandboxNodeVisitor;
+use Twig\Sandbox\SecurityNotAllowedMethodError;
+use Twig\Sandbox\SecurityNotAllowedPropertyError;
+use Twig\Sandbox\SecurityPolicyInterface;
+use Twig\Sandbox\SourcePolicyInterface;
+use Twig\Source;
+use Twig\TokenParser\SandboxTokenParser;
+
+final class SandboxExtension extends AbstractExtension
+{
+    private $sandboxedGlobally;
+    private $sandboxed;
+    private $policy;
+    private $sourcePolicy;
+
+    public function __construct(SecurityPolicyInterface $policy, $sandboxed = false, SourcePolicyInterface $sourcePolicy = null)
+    {
+        $this->policy = $policy;
+        $this->sandboxedGlobally = $sandboxed;
+        $this->sourcePolicy = $sourcePolicy;
+    }
+
+    public function getTokenParsers()
+    {
+        return [new SandboxTokenParser()];
+    }
+
+    public function getNodeVisitors()
+    {
+        return [new SandboxNodeVisitor()];
+    }
+
+    public function enableSandbox()
+    {
+        $this->sandboxed = true;
+    }
+
+    public function disableSandbox()
+    {
+        $this->sandboxed = false;
+    }
+
+    public function isSandboxed(Source $source = null)
+    {
+        return $this->sandboxedGlobally || $this->sandboxed || $this->isSourceSandboxed($source);
+    }
+
+    public function isSandboxedGlobally()
+    {
+        return $this->sandboxedGlobally;
+    }
+
+    private function isSourceSandboxed(?Source $source): bool
+    {
+        if (null === $source || null === $this->sourcePolicy) {
+            return false;
+        }
+
+        return $this->sourcePolicy->enableSandbox($source);
+    }
+
+    public function setSecurityPolicy(SecurityPolicyInterface $policy)
+    {
+        $this->policy = $policy;
+    }
+
+    public function getSecurityPolicy()
+    {
+        return $this->policy;
+    }
+
+    public function checkSecurity($tags, $filters, $functions, Source $source = null)
+    {
+        if ($this->isSandboxed($source)) {
+            $this->policy->checkSecurity($tags, $filters, $functions);
+        }
+    }
+
+    public function checkMethodAllowed($obj, $method, int $lineno = -1, Source $source = null)
+    {
+        if ($this->isSandboxed($source)) {
+            try {
+                $this->policy->checkMethodAllowed($obj, $method);
+            } catch (SecurityNotAllowedMethodError $e) {
+                $e->setSourceContext($source);
+                $e->setTemplateLine($lineno);
+
+                throw $e;
+            }
+        }
+    }
+
+    public function checkPropertyAllowed($obj, $property, int $lineno = -1, Source $source = null)
+    {
+        if ($this->isSandboxed($source)) {
+            try {
+                $this->policy->checkPropertyAllowed($obj, $property);
+            } catch (SecurityNotAllowedPropertyError $e) {
+                $e->setSourceContext($source);
+                $e->setTemplateLine($lineno);
+
+                throw $e;
+            }
+        }
+    }
+
+    public function ensureToStringAllowed($obj, int $lineno = -1, Source $source = null)
+    {
+        if ($this->isSandboxed($source) && \is_object($obj) && method_exists($obj, '__toString')) {
+            try {
+                $this->policy->checkMethodAllowed($obj, '__toString');
+            } catch (SecurityNotAllowedMethodError $e) {
+                $e->setSourceContext($source);
+                $e->setTemplateLine($lineno);
+
+                throw $e;
+            }
+        }
+
+        return $obj;
+    }
+}
+
+class_alias('Twig\Extension\SandboxExtension', 'Twig_Extension_Sandbox');
